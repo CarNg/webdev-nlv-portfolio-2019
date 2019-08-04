5c5c3060cd78a4bd572b070e3c250322da9c9299
CarNg <CarNg@users.noreply.github.com>
2019-08-04 19:48:16 +0800

    Cleaned up some CSS

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -8,7 +8,7 @@ export default function ContactForm() {
             <input type="text" placeholder="your name" className={style.formInput} />
             <input type="email" placeholder="your email" className={style.formInput} />
             <textarea type="textarea" placeholder="your message" className={style.formInput} rows="4" style={{resize: "none"}} />
-            <div className={[style.button, "button"].join(' ')} style={{marginLeft: "0px"}}>
+            <div className={[style.button, "button"].join(' ')}>
                 send
             </div>
         </div>
