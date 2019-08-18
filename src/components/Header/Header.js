ac0bddfb948923184f5ef4091e2991ad128c9f65
CarNg <CarNg@users.noreply.github.com>
2019-08-18 22:29:17 +0800

    Added temp mobile template

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,36 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import MediaQuery from 'react-responsive';
 //CSS
 import style from './Header.module.css';
 //Images
 import logo from '../../assets/images/logo.png';
 
 function Header() {
+	const mobileHeader = {
+		width: "150px",
+		padding: "20px",
+		margin: "0 auto",
+	}
+
   	return (
 		<header>
-			<div className={style.headerContainer}>
-				<Link to ="/"><img className={style.logo} src={logo} alt="Night Light Visuals Logo"/></Link>
-				<Link to="contact" className={style.headerLink}>Contact</Link>
-				<Link to="projects" className={style.headerLink}>Projects</Link>
-				<Link to="about" className={style.headerLink}>About</Link>
-				<Link to="/" className={style.headerLink}>Welcome</Link>
-			</div>
+			<MediaQuery query="(min-device-width: 768px)">
+				<div className={style.headerContainer}>
+					<Link to ="/"><img className={style.logo} src={logo} alt="Night Light Visuals Logo"/></Link>
+					<Link to="contact" className={style.headerLink}>Contact</Link>
+					<Link to="projects" className={style.headerLink}>Projects</Link>
+					<Link to="about" className={style.headerLink}>About</Link>
+					<Link to="/" className={style.headerLink}>Welcome</Link>
+				</div>
+			</MediaQuery>
+			<MediaQuery query="(max-device-width: 767px)">
+				<div style={mobileHeader}>
+					<Link to ="/">
+						<img alt="LinkedIn Icon" src={logo} width="100%" height="100%"/>
+					</Link>
+				</div>
+			</MediaQuery>
 		</header>
   );
 }
--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -2,25 +2,118 @@ import React from 'react'
 import style from './Welcome.module.css';
 import {Link} from 'react-router-dom'
 import resume from '../../assets/files/CarmenNgResume.pdf'
+import MediaQuery from 'react-responsive';
+//Images
+import socialYoutube from '../../assets/images/socialYoutube.png';
+import socialLinkedIn from '../../assets/images/socialLinkedIn.png';
+import socialSociety6 from '../../assets/images/socialSociety6.png';
+import socialItch from '../../assets/images/socialItch.png';
+import socialGithub from '../../assets/images/socialGithub.png'
 
 export default function Welcome() {
+    //Temp mobile css
+    const mobileTitle = {
+        fontSize: "5rem",
+        borderTop: "3px solid var(--nlv-cyan)",
+        borderBottom: "3px solid var(--nlv-cyan)",
+    }
+
+    const mobileSubtitle = {
+        marginTop: "20px",
+        fontSize: "1.5rem"
+    }
+
+    const socialRow = {
+        width: "100%",
+        display: "table",
+        tableLayout: "fixed"
+    }
+
+    const socialIcon = {
+        width: "40px",
+        height: "40px",
+        padding: "10px"
+    }
+
+    const socialColumn = {
+        display: "table-cell"
+    }
+
     return (
         <div className={style.welcomeWrapper}>
-            <h1>Carmen Ng</h1>
-            <h3 style={{paddingRight: "80px"}}>Web Developer<span style={{color: "var(--nlv-red)", fontSize: "4rem"}}>/</span>Game Designer</h3>
-            <h5>Originally based in Ireland, now based in Hong Kong. Orginally a web designer, now a web developer.</h5>
-            <div id={style.buttonsWrapper}>
-                <a href={resume} target="_blank" rel="noopener noreferrer">
-                    <div className={[style.button, "button"].join(' ')} style={{marginLeft: "0px"}}>
-                        view resume
+            <MediaQuery query="(min-device-width: 768px)">
+                    <h1>Carmen Ng</h1>
+                    <h3 style={{paddingRight: "80px"}}>Web Developer<span style={{color: "var(--nlv-red)", fontSize: "4rem"}}>/</span>Game Designer</h3>
+                    <h5>Originally based in Ireland, now based in Hong Kong. Orginally a web designer, now a web developer.</h5>
+                    <div id={style.buttonsWrapper}>
+                        <a href={resume} target="_blank" rel="noopener noreferrer">
+                            <div className={[style.button, "button"].join(' ')} style={{marginLeft: "0px"}}>
+                                view resume
+                            </div>
+                        </a>
+                        <Link to="/projects">
+                            <div className={[style.button, "button"].join(' ')}>
+                                view projects
+                            </div>
+                        </Link>
                     </div>
-                </a>
-                <Link to="/projects">
-                    <div className={[style.button, "button"].join(' ')}>
-                        view projects
+            </MediaQuery>
+            <MediaQuery query="(max-device-width: 767px)">
+                    <div style={mobileTitle}>STAY TUNED</div>
+                    <div style={mobileSubtitle}>
+                        Mobile site is coming very soon!
+                        <br/><br/><br/>
+                        For now, check out the desktop site 
+                        <br/> or follow along:
                     </div>
-                </Link>
-            </div>
+                    <div style={socialRow}>
+                        <a href="https://github.com/CarNg/" style={socialColumn}>
+                            <div style={socialIcon}>
+                                <img
+                                    alt="Github Icon"
+                                    src={socialGithub} 
+                                    width="100%"
+                                />
+                            </div>
+                        </a>
+                        <a href="https://nightlightvisuals.itch.io/" style={socialColumn}>
+                            <div style={socialIcon}>
+                                    <img
+                                        alt="Itch.io Icon"
+                                        src={socialItch} 
+                                        width="100%"
+                                    />
+                            </div>
+                        </a>  
+                        <a href="https://www.linkedin.com/in/carng" style={socialColumn}>
+                            <div style={socialIcon}>
+                                <img
+                                    alt="LinkedIn Icon"
+                                    src={socialLinkedIn} 
+                                    width="100%"
+                                />
+                            </div>
+                        </a>
+                        <a href="https://www.youtube.com/channel/UCVcdn5DAeuBsahRK4Gk5XmA" style={socialColumn}>
+                            <div style={socialIcon}>
+                                <img
+                                    alt="Youtube Icon"
+                                    src={socialYoutube} 
+                                    width="100%"
+                                />  
+                            </div>
+                        </a>
+                        <a href="https://society6.com/carmenng" style={socialColumn}>
+                            <div style={socialIcon}>
+                                <img
+                                    alt="Society6 Icon"
+                                    src={socialSociety6} 
+                                    width="100%"
+                                />
+                            </div>
+                        </a>
+                    </div> 
+            </MediaQuery>
         </div>
     )
 }
