19bf3e395b5c3e449167a3d42581b4eaa0cbe5d0
CarNg <CarNg@users.noreply.github.com>
2019-09-02 20:43:09 +0800

    Updated skills section of about

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import style from '../Profile/Profile.module.css'
 import profile from '../../assets/images/profile.jpg'
+import MediaQuery from 'react-responsive';
 
 class Profile extends React.PureComponent {
     constructor(props){
@@ -19,7 +20,12 @@ class Profile extends React.PureComponent {
                     <img src={profile} alt="Night Light Visuals profile" onLoad={() => {this.imageLoadedHandler()}} />
                     <div id={style.profileText}>
                         <p>
-                            <span style={{color: "var(--nlv-cyan)", fontSize: "1.8rem"}}>Hi! I'm Carmen. Welcome to my portfolio!</span> 
+                            <MediaQuery query="(min-device-width: 768px)"> 
+                                <span style={{color: "var(--nlv-cyan)", fontSize: "1.8rem"}}>Hi! I'm Carmen. Welcome to my portfolio!</span> 
+                            </MediaQuery>
+                            <MediaQuery query="(max-device-width: 767px)"> 
+                                <span style={{color: "var(--nlv-cyan)", fontSize: "1.8rem"}}>Hi! I'm Carmen.<br/>Welcome to my portfolio!</span> 
+                            </MediaQuery>
                             <br/>
                             I'm a web developer by profession and have been for the last 3 years. With my viscom background, I focus primarly on front-end development but I've done some full stack work too. I'm also an avid board game player and game designer so you'll find some game projects too. I'm always eager to learn new skills and gain experience so stay tuned while I continue to grow this portfolio and my resume.
                         </p>
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,38 +1,81 @@
 import React from 'react'
 import style from '../Skills/Skills.module.css'
+import MediaQuery from 'react-responsive';
 
 export default function Skills() {
     return (
-        <div id={style.skillsWrapper}>            
-            <ul className={style.skillList}>
-                <li>
-                    <em>HTML5</em>
-                    <span className={`${style.expand} ${style.html5}`}></span>
-                </li>
-                <li>
-                    <em>CSS3</em>
-                    <span className={`${style.expand} ${style.css3}`}></span>
-                </li>
-                <li>
-                    <em>JavaScript</em>
-                    <span className={`${style.expand} ${style.javascript}`}></span>
-                </li>
-            </ul>
+        <React.Fragment>
+            <MediaQuery query="(min-device-width: 768px)"> 
+                <div id={style.skillsWrapper}>            
+                    <div className={style.skillList}>
+                        <span className={style.skillTitle}>Projects</span>
+                            <br/>
+                            Custom ERP Sites<br/>
+                            POS Web App<br/>
+                            OpenCart Sites<br/>
+                            iOS Games<br/>
+                            Board Games
+                            <br/>
+                    </div>
+    
+                    <div className={style.skillList}>
+                        <span className={style.skillTitle}>Languages</span>
+                            <br/>
+                            HTML5<br/>
+                            CSS3<br/>
+                            JavaScript<br/>
+                            English<br/>
+                            Cantonese
+                            <br/>
+                    </div>
+    
+                    <div className={style.skillList}>
+                        <span className={style.skillTitle}>Tools</span>
+                            <br/>
+                            React.js<br/>
+                            Bootstrap<br/>
+                            jQuery<br/>
+                            Github<br/>
+                            Photoshop
+                            <br/>
+                    </div>
+                </div> 
+            </MediaQuery>    
+        </React.Fragment>
 
-            <ul className={style.skillList}>
-                <li>
-                    <em>ReactJS</em>
-                    <span className={`${style.expand} ${style.reactjs}`}></span>
-                </li>
-                <li>
-                    <em>jQuery</em>
-                    <span className={`${style.expand} ${style.jquery}`}></span>
-                </li>
-                <li>
-                    <em>Bootstrap</em>
-                    <span className={`${style.expand} ${style.bootstrap}`}></span>
-                </li>
-            </ul>
-        </div>
+        
+
+        //PREVIOUSLY USED ANIMATED SKILLS
+        // <div id={style.skillsWrapper}>            
+        //     <ul className={style.skillList}>
+        //         <li>
+        //             <em>HTML5</em>
+        //             <span className={`${style.expand} ${style.html5}`}></span>
+        //         </li>
+        //         <li>
+        //             <em>CSS3</em>
+        //             <span className={`${style.expand} ${style.css3}`}></span>
+        //         </li>
+        //         <li>
+        //             <em>JavaScript</em>
+        //             <span className={`${style.expand} ${style.javascript}`}></span>
+        //         </li>
+        //     </ul>
+
+        //     <ul className={style.skillList}>
+        //         <li>
+        //             <em>ReactJS</em>
+        //             <span className={`${style.expand} ${style.reactjs}`}></span>
+        //         </li>
+        //         <li>
+        //             <em>jQuery</em>
+        //             <span className={`${style.expand} ${style.jquery}`}></span>
+        //         </li>
+        //         <li>
+        //             <em>Bootstrap</em>
+        //             <span className={`${style.expand} ${style.bootstrap}`}></span>
+        //         </li>
+        //     </ul>
+        // </div>
     )
 }
